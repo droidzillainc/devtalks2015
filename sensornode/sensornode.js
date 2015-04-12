a4ea7181424c9ca0e3914c0aa23a59433359b42c
Andrei CRACIUN <andrei@craciun.name>
2015-04-12 12:27:12 +0000

    wip

--- sensornode/sensornode.js
+++ sensornode/sensornode.js
@@ -1,27 +1,40 @@
 var iotkit = require('iotkit-comm');
 var path = require('path');
-var index = 0;
+//var index = 0;
 
 var name = process.argv[2];
 if (name == undefined){
     name = "temperature-in";
 }
 
+name = "/senzoriada/"+name+"/sensornode";
+
 var updateTime = process.argv[3];
 if (updateTime == undefined) {
     updateTime = 5000;
 }
 
+
 console.log("using:"+name+ " updateTime:"+ updateTime);
 
-var spec = new iotkit.ServiceSpec({"name":"/senzoriada/"+name+"/sensornode", "type":{"name":"mqtt"}});
+var spec = new iotkit.ServiceSpec({"name":name, "type":{"name":"mqtt"}});
 
 iotkit.createService(spec, function (service) {
   setInterval(function() {
     console.log("Sending data");
-    var sensorData = {"temperature":12.43, "battery":3200, "index":index++};
+    var sensorData = {
+			"id":name,
+			"voltage":4500,
+			"timestamp":(new Date()).getTime(),
+			"sensors":[
+			    {
+				"type":10,
+				"version":1,
+				"value":2376
+			    }
+			]};
     service.comm.send(JSON.stringify(sensorData));
-    console.log("Data send."+ index);
+//    console.log("Data send."+ index);
   }, updateTime);
 });
 
